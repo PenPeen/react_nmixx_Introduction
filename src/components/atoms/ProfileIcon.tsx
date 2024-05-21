6cbd3787d463773c2a821b4b8f42b01a858494ce
Pen Peen <ikegami1022.h@gmail.com>
2024-05-21 21:48:00 +0900

    :zap: width, heightは動的に指定できるようにして拡張性を上げる

--- src/components/atoms/ProfileIcon.tsx
+++ src/components/atoms/ProfileIcon.tsx
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-type Props = { image_path?: string };
+type Props = { width: number; height: number; image_path?: string };
 
 const DEFAULT_ICON = '/default_icon.svg';
 
-const Icon = styled.img`
-  width: 75px;
-  height: 75px;
+const Icon = styled.img<Props>`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
   border-radius: 50%;
   background-color: #ffcccb;
   border: none;
@@ -23,12 +23,18 @@ const Icon = styled.img`
   }
 `;
 
-const ProfileIcon: React.FC<Props> = ({ image_path = DEFAULT_ICON }) => {
-  return <Icon src={image_path} alt="profile" />;
+const ProfileIcon: React.FC<Props> = ({
+  image_path = DEFAULT_ICON,
+  height,
+  width,
+}) => {
+  return <Icon src={image_path} alt="profile" height={height} width={width} />;
 };
 
 ProfileIcon.propTypes = {
-  image_path: PropTypes.string.isRequired,
+  image_path: PropTypes.string,
+  height: PropTypes.number.isRequired,
+  width: PropTypes.number.isRequired,
 };
 
 export default ProfileIcon;
